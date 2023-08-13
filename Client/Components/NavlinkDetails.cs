cdc70ecddfb160b98e884201243fc5d6cab96fd6
Rielk <william@symbotics.co.uk>
2023-08-12 20:47:58 -0400

    Links now collapse into a dropdown menu.

---
+++ Client/Components/NavlinkDetails.cs
@@ -0,0 +1,13 @@
+﻿public class NavLinkDetails
+{
+	public NavLinkDetails(string href, string content, bool matchAll = false)
+	{
+		HRef = href;
+		Content = content;
+		MatchAll = matchAll;
+	}
+
+	public string HRef { get; set; }
+	public string Content { get; set; }
+	public bool MatchAll { get; set; }
+}
