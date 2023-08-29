a5fe461d02ed2cfba185b2a7cf09e22f70408393
William Sullivan <william@symbotics.co.uk>
2023-08-29 17:53:56 -0400

    Fixed TopLinks format

--- Client/Components/TopLinks.razor.cs
+++ Client/Components/TopLinks.razor.cs
@@ -1,13 +1,11 @@
 ﻿public class NavLinkDetails
 {
-	public NavLinkDetails(string href, string content, bool matchAll = false)
+	public NavLinkDetails(string href, string content)
 	{
 		HRef = href;
 		Content = content;
-		MatchAll = matchAll;
 	}
 
 	public string HRef { get; set; }
 	public string Content { get; set; }
-	public bool MatchAll { get; set; }
 }
