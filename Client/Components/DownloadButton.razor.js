a423d0b28c95f1d07deed40c54d88e50510901c3
William Sullivan <william@symbotics.co.uk>
2023-08-29 13:12:15 -0400

    Download done the simple way.

--- Client/Components/DownloadButton.razor.js
+++
@@ -1,7 +0,0 @@
-﻿export function triggerFileDownload(fileName, url) {
-    const anchorElement = document.createElement('a');
-    anchorElement.href = url;
-    anchorElement.download = fileName ?? '';
-    anchorElement.click();
-    anchorElement.remove();
-}
