3ac4b801553c0c512516928c602b3348ade16446
William Sullivan <william@symbotics.co.uk>
2023-09-16 14:57:38 -0400

    Moved common HTML elements to the layout. And added defaults for the MainLayoutSetter.

--- Client/Shared/MainLayout.razor.cs
+++ Client/Shared/MainLayout.razor.cs
@@ -4,19 +4,23 @@ namespace BlazorApp.Client.Shared;
 
 public class MainLayoutSetter : ComponentBase
 {
-    [CascadingParameter(Name = "Layout")]
-    public MainLayout Layout { get; set; } = null!;
+	[CascadingParameter(Name = "Layout")]
+	public MainLayout Layout { get; set; } = null!;
 
-    [Parameter]
-    public RenderFragment? Hero { get; set; }
+	[Parameter]
+	public RenderFragment? Hero { get; set; }
 
-    [Parameter]
-    public List<LinkDetails>? Links { get; set; }
+	[Parameter]
+	public List<LinkDetails>? Links { get; set; }
 
-    protected override void OnInitialized()
-    {
-        Layout.SetHero(Hero);
-        Layout.SetLinks(Links);
-        base.OnInitialized();
-    }
+	[Parameter]
+	public bool EndBGLighter { get; set; } = true;
+
+	protected override void OnInitialized()
+	{
+		Layout.SetHero(Hero);
+		Layout.SetLinks(Links);
+		Layout.SetEndBGLighter(EndBGLighter);
+		base.OnInitialized();
+	}
 }
