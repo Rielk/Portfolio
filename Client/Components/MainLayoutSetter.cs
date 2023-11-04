71ed7bee1eb68d7c0b5eec8b630d182e3158986f
William Sullivan <william@symbotics.co.uk>
2023-11-04 00:42:59 -0400

    Moved components to their own file

---
+++ Client/Components/MainLayoutSetter.cs
@@ -0,0 +1,35 @@
+﻿using BlazorApp.Client.Shared;
+using Microsoft.AspNetCore.Components;
+
+namespace BlazorApp.Client.Components;
+
+public class MainLayoutSetter : ComponentBase
+{
+	protected override bool ShouldRender() => false;
+
+	[CascadingParameter(Name = "Layout")]
+	public MainLayout Layout { get; set; } = null!;
+
+	[Parameter]
+	public RenderFragment? DisplayHero { get; set; }
+
+	[Parameter]
+	public List<LinkDetails>? Links { get; set; }
+
+	[Parameter]
+	public bool EndBGLighter { get; set; } = true;
+
+	protected override void OnInitialized()
+	{
+		Layout.SetHero(DisplayHero);
+		Layout.SetLinks(Links);
+		Layout.SetEndBGLighter(EndBGLighter);
+		base.OnInitialized();
+	}
+
+	protected override void OnParametersSet()
+	{
+		Layout.SetEndBGLighter(EndBGLighter);
+		base.OnParametersSet();
+	}
+}
