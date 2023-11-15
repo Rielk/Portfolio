34d574af2ea0e5a04b94ba7758e6368e5cf81251
William Sullivan <william@symbotics.co.uk>
2023-11-15 02:24:22 -0500

    Removed tsonfig.json

--- Client/generatedJava/wwwroot/js/browserResize.js
+++
@@ -1,9 +0,0 @@
-window.browserResize = {
-    registerResizeCallback: function () {
-        window.addEventListener("resize", browserResize.resized);
-    },
-    resized: function () {
-        DotNet.invokeMethodAsync("Client", 'OnBrowserResize').then(function (data) { return data; });
-    }
-};
-//# sourceMappingURL=browserResize.js.map
